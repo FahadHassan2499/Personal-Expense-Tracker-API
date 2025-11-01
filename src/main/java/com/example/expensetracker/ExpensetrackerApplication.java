6555d9ccd2a48e052e837df77cd1fa1205791757
FahadHassan2499 <fahadhassan2499@gmail.com>
2025-11-01 15:01:37 +0530

    Configured MySQL for deployment

--- src/main/java/com/example/expensetracker/ExpensetrackerApplication.java
+++ src/main/java/com/example/expensetracker/ExpensetrackerApplication.java
@@ -2,11 +2,18 @@ package com.example.expensetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ExpensetrackerApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
 		SpringApplication.run(ExpensetrackerApplication.class, args);
 	}
 
