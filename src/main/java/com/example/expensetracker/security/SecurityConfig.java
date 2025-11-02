6b5e4174aa4379e9e75220c3b54c1e5e951af831
FahadHassan2499 <fahadhassan2499@gmail.com>
2025-11-02 16:51:14 +0530

    Added /add after frontend url

--- src/main/java/com/example/expensetracker/security/SecurityConfig.java
+++ src/main/java/com/example/expensetracker/security/SecurityConfig.java
@@ -26,7 +26,6 @@ public class SecurityConfig {
                         // allow preflight requests
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/api/expenses/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
